162a8083287eafaca8a73df6de8307681f6dd34e
Craig Reay <craigcsruk@googlemail.com>
2016-10-29 20:05:29 +0100

    Removed commented out code and added a complete log

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = function (context, myBlob) {
     context.log('Starting...');
-    //context.log('Context: ', context);
     context.log('File: ', context.bindingData.blobTrigger);
     context.log('Url: ', process.env.StorageUri + context.bindingData.blobTrigger);
-    //context.log('Node.js blob trigger function processed blob:', myBlob);
+	context.log('Complete.');
     context.done();
 };
