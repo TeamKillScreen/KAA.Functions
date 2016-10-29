4705567e9c68c39f4ed26f0992699f5824677f60
Craig Reay <craigcsruk@googlemail.com>
2016-10-29 23:18:28 +0100

    Added missing persons function

---
+++ NewMissingPerson/index.js
@@ -0,0 +1,60 @@
+var https = require('https');
+
+module.exports = function (context, req) {
+    context.log('Starting...');
+    
+	var filePath = req.FilePath;
+	var fileUrl = process.env.StorageUri + filePath;
+	
+	context.log('File: ', filePath);
+    context.log('Url: ', fileUrl);
+	
+	var cognitiveServicePath = "/face/v1.0/facelists/missingpersons/persistedFaces"
+	var ocpApimSubscriptionKey = process.env.OcpApimSubscriptionKey;
+	
+	var post_data = JSON.stringify({
+    	url: fileUrl
+	})
+	
+	context.log('Service Url', process.env.CognitiveServiceUri);
+	context.log('Path: ', cognitiveServicePath);
+	context.log('Post Data: ', post_data);
+	
+	// An object of options to indicate where to post to
+	var post_options = {
+		host: process.env.CognitiveServiceUri,
+		port: '443',
+		path: cognitiveServicePath,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Ocp-Apim-Subscription-Key' : ocpApimSubscriptionKey,
+			'Content-Length': Buffer.byteLength(post_data),
+		}
+	};
+	
+	var body = ''
+	// Set up the request
+	var post_req = https.request(post_options, function(res) {
+		res.setEncoding('utf8');
+		
+		res.on('data', function (chunk) {
+			body += chunk;
+		});
+		
+		res.on('end', function () {
+    		context.log(body);
+  		});
+	});
+	
+	post_req.on('error', function(e) {
+		context.log('problem with request: ' + e.message);
+	});
+	
+	// post the data
+	post_req.write(post_data);
+	post_req.end();
+	
+	context.log('Complete.');
+    context.done();
+};
