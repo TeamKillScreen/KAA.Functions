ca93e8c13641ac23b00a1dd288615b31c1a439a6
Craig Reay <craigcsruk@googlemail.com>
2016-10-30 02:22:36 +0000

    Added a delete function

---
+++ DeleteMissingPerson/index.js
@@ -0,0 +1,59 @@
+var https = require('https');
+
+module.exports = function (context, req) {
+    context.log('Starting...');
+    
+	var filePath = req.FilePath;
+	var personId = req.personId;
+	var fileUrl = process.env.StorageUri + filePath;
+	
+	context.log('File: ', filePath);
+    context.log('Url: ', fileUrl);
+	
+	var cognitiveServicePath = "/face/v1.0/facelists/missingpersons/persistedFaces/" + req.persistedFaceId;
+	var ocpApimSubscriptionKey = process.env.OcpApimSubscriptionKey;
+	
+	var post_data = JSON.stringify({
+    	url: fileUrl
+	})
+	
+	context.log('Service Url', process.env.CognitiveServiceUri);
+	context.log('Path: ', cognitiveServicePath);
+	context.log('Post Data: ', post_data);
+	
+	// An object of options to indicate where to post to
+	var post_options = {
+		host: process.env.CognitiveServiceUri,
+		port: '443',
+		path: cognitiveServicePath,
+		method: 'DELETE',
+		headers: {
+			'Ocp-Apim-Subscription-Key' : ocpApimSubscriptionKey
+		}
+	};
+	
+	var body = ''
+	// Set up the request
+	var post_req = https.request(post_options, function(res) {
+		res.setEncoding('utf8');
+		
+		res.on('data', function (chunk) {
+			body += chunk;
+		});
+		
+		res.on('end', function () {
+    		context.log(body);
+			context.log('Deleted persistedFaceId', req.persistedFaceId);
+			context.done();
+  		});
+	});
+	
+	post_req.on('error', function(e) {
+		context.log('problem with request: ' + e.message);
+		context.done();
+	});
+	
+	// post the data
+	post_req.write(post_data);
+	post_req.end();
+};
